8cc649d5569de19c67649caef0b557a8a29f5622
Zach Posten <zposten@gmail.com>
2019-01-25 14:23:52 -0600

    feat: Add routing to the application

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,13 +2,8 @@ import {Component} from '@angular/core'
 
 @Component({
   selector: 'pm-root',
-  template: `
-    <div>
-      <h1>{{pageTitle}}</h1>
-      <pm-products></pm-products>
-    </div>
-  `,
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
-  pageTitle = 'Acme Product Management'
+  public pageTitle = 'Acme Product Management'
 }
--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -2,12 +2,14 @@ import {BrowserModule} from '@angular/platform-browser'
 import {NgModule} from '@angular/core'
 import {FormsModule} from '@angular/forms'
 import {HttpClientModule} from '@angular/common/http'
+import {RouterModule} from '@angular/router'
 
 import {AppComponent} from './app.component'
 import {ProductListComponent} from './products/product-list.component'
 import {ConvertCharToSpacePipe} from './util/convertCharToSpacePipe'
 import {StarComponent} from './shared/star.component'
 import {TimesPipe} from './util/timesPipe'
+import {WelcomeComponent} from './home/welcome.component'
 
 @NgModule({
   declarations: [
@@ -16,11 +18,18 @@ import {TimesPipe} from './util/timesPipe'
     StarComponent,
     ConvertCharToSpacePipe,
     TimesPipe,
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'products', component: ProductListComponent},
+      {path: 'welcome', component: WelcomeComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '**', redirectTo: 'welcome'},
+    ]),
   ],
   bootstrap: [AppComponent],
 })
