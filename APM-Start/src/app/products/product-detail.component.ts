6584ab087b64f38d2f32ed5f92a665bad4ca7c17
Zach Posten <zposten@gmail.com>
2019-01-28 11:16:48 -0600

    feat: Add a back button to the product detail page

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,17 +1,35 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { IProduct } from './product';
+import {Component, OnInit} from '@angular/core'
+import {IProduct} from './product'
+import {ActivatedRoute, Router} from '@angular/router'
+import {ProductService} from './products.service'
 
 @Component({
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
 })
 export class ProductDetailComponent implements OnInit {
-  @Input() public product: IProduct
+  public product: IProduct
+  public pageTitle = 'Product detail'
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductService,
+    private router: Router,
+  ) {
 
+  }
 
   public ngOnInit() {
+    let productId = +this.route.snapshot.paramMap.get('id')
+    this.productsService
+      .getProducts()
+      .subscribe(products => {
+        this.product = products.find(p => p.id === productId)
+      })
+  }
+
+  public goBack() {
+    this.router.navigate(['products'])
   }
 
 }
